18f92368b0f9e261d92a4307dfe05adb115c4248
concipaulo <pauloagconci@gmail.com>
2017-11-05 20:15:42 -0200

    recent update

--- lock.sh
+++ lock.sh
@@ -5,6 +5,6 @@ convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 # convert /tmp/screen.png -paint 3 -modulate 80 /tmp/screen.png
 # convert /tmp/screen.png -paint 3 /tmp/screen.png
 [[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png  ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
-playerctl pause
+# playerctl pause
 i3lock -e -i /tmp/screen.png
 rm /tmp/screen.png
