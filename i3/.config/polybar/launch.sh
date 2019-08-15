d1d8857aefef50864a3a285aa293d94aae009888
concipaulo <vedat@saturn>
2019-08-15 04:58:48 -0300

    polybar improvements

--- i3/.config/polybar/launch.sh
+++ i3/.config/polybar/launch.sh
@@ -4,9 +4,9 @@
 killall -q polybar >/dev/null
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar 2>/dev/null; do sleep 1; done
 
 # Launch polybar
 polybar simple & 
 
-notify-send "Polybar launched..."
+notify-send -u low "Polybar launched..."
